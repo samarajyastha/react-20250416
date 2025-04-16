d6c7a17e814970bf2628bdecfe8f80c9822d899e
Samarajya Shrestha <samarajyastha@gmail.com>
2025-04-16 21:16:24 +0545

    feat: init react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <main className="py-20 px-8">
+      <div className="container max-w-screen-xl mx-auto py-10 px-14 bg-slate-100 shadow-lg rounded-2xl">
+        <Title />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+  return <h1 className="text-zinc-800 text-5xl text-center font-semibold">Todo App</h1>;
+}
+
+export default Title;
