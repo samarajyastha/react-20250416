b372bc4c9f61baab80eb0fd4dac8f542aee64f65
Samarajya Shrestha <samarajyastha@gmail.com>
2025-04-18 20:38:45 +0545

    feat: implement pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Todo App",
+        short_name: "TodoApp",
+        start_url: "/",
+        icons: [
+          {
+            src: "star.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
